5c2c01447debebd700193f0400b8f33ed423c0f4
Muhammad Faraz Abbasi <muhammadfaraz.abbasi786@gmail.com>
2025-08-01 00:20:51 +0500

    admin login issue fix

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -241,8 +241,10 @@ const adminLogin = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      // secure: process.env.NODE_ENV === "production",
+      secure: true,
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
